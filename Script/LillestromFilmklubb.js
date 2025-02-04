c98821f751a68948a846edbcb19f416c86e607ba
Morten Løpen <morten.lopen@outlook.com>
2025-02-04 23:20:35 +0100

    Logo links and best practises fixes

--- Script/LillestromFilmklubb.js
+++ Script/LillestromFilmklubb.js
@@ -1,1 +1,7 @@
-﻿
+﻿function openFilmmeny() {
+	var dropdown = document.getElementById("filmmeny-dropdown");
+	var selectedValue = dropdown.value;
+	if (selectedValue) {
+		window.location.href = selectedValue;
+	}
+}
